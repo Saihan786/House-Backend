7b93fa7bfc3d326eb3f84192239e9b6cdf464fdb
Saihan786 <marshall.s15@hotmail.com>
2024-05-21 10:41:14 +0100

    Add folium map * Can draw polygons (red line plots) and save them using Export as a .geojson file

--- gpexample.py
+++ gpexample.py
@@ -2,22 +2,19 @@ import geopandas
 from geodatasets import get_path
 import geopandas.datasets
 from matplotlib import pyplot as plt
+import folium as folium
+from folium.plugins import Draw
 import webbrowser
 import os
 
-
-path_to_data = get_path("nybb")
-world = geopandas.read_file(geopandas.datasets.get_path("naturalearth_lowres"))
-gdf = geopandas.read_file(path_to_data)
-
-gdf = gdf.set_index("BoroName")
-gdf["boundary"] = gdf.boundary
-gdf["centroid"] = gdf.centroid
-gdf["distance"] = gdf["centroid"].distance(gdf["centroid"].iloc[0])
-gdf["area"] = gdf.area
+# path_to_rlp = "C:/Users/Saihan Marshall/Documents/house stuff/house repo/House/data.geojson"
+# rlp = geopandas.read_file(path_to_rlp)
+# print(rlp)
+# rlp.plot()
 
 
-fmap = gdf.explore("area", legend=True)
+fmap = folium.Map()
+Draw(export=True).add_to(fmap)
 fmap.save('map.html')
 webbrowser.open('file://' + os.path.realpath('map.html'))
 plt.show()
