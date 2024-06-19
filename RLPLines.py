92bc9639832ba678c62f5f6d78807b2ee3b8bf3c
Saihan786 <marshall.s15@hotmail.com>
2024-06-19 04:24:16 +0100

    Update README for constraints and plotting

--- RLPLines.py
+++ RLPLines.py
@@ -1,25 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import geopandas
-from AddDataToDF import coords
 from RedLinePlot import getRLP, getPath
 from shapely import Polygon
-
-oldcoords = coords()
-
-def findLineFromTuples(p1, p2):
-    x1,y1 = p1
-    x2,y2 = p2
-    if not (x2-x1)==0:
-        m = (y2-y1) / (x2-x1)
-        c = y1-(m*x1)
-    else:
-        # add edge case for x2-x1==0
-        # for now, causes error as m and c haven't been initialised so cannot be returned
-        pass    
-    return (m, c)
-
-
 
 rlp = getRLP(getPath())
 rlp = rlp.to_crs(epsg=27700)
@@ -32,15 +15,16 @@ ax = rlp.plot(facecolor="grey")
 geopandas.GeoSeries(oblong).plot(ax=ax, facecolor="red")
 
 print(oblong.within(rlppolygon))
+print(rlppolygon.contains(oblong))
+
+
+
 
 
 
 
 # for plotting purposes only - cannot check if a point or polygon is within a ring
 rlpRing = rlp.exterior
-
-
-
 
 
 plt.show()
