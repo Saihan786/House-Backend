e0436167552cb8631a18ffb0fd120d43273ed826
Saihan786 <marshall.s15@hotmail.com>
2024-07-15 18:37:12 +0100

    Add else case for pathfinding in polygon *Separated method for clarity and generalisation

--- PolygonFunctions.py
+++ PolygonFunctions.py
@@ -59,7 +59,58 @@ def findAdjacentLine(polygon):
     return (lines[adjacentlineindex].coords, corner)
 
 
-def findAdjacentLines(polygon):
+def find_path(coords, startpoint, endpoint, longestline, pathIsHorizontal, shouldContainLL):
+        """Returns a list of lines as coordinates between the given points.
+        
+        The path returned may contain the longest line.
+        
+        'pathIsHorizontal' is information provided to the method about the existing path, not a
+        setting about the path it generates itself.
+
+        'should contain longest line', however, is a setting about the path the user can choose
+        to activate for the line.
+        
+        """
+        
+        if pathIsHorizontal:
+            if longestline[linep2idx][X] < longestline[linep1idx][X]:
+                longestline = [ longestline[linep2idx], longestline[linep1idx] ]
+        else:
+            if longestline[linep2idx][Y] > longestline[linep1idx][Y]:
+                longestline = [ longestline[linep2idx], longestline[linep1idx] ]
+
+
+        if startpoint > endpoint:
+            startpointfirst = coords[startpoint:-1] + [coords[-1]] + coords[0:startpoint]
+        else:
+            startpointfirst = coords[startpoint:endpoint+1]
+
+
+        containsLongestLine = False
+        for i in range(-1+len(startpointfirst)):
+            line = [startpointfirst[i], startpointfirst[i+1]]
+            if line==[c for c in longestline]:
+                containsLongestLine = True
+                break
+                
+        if ( (shouldContainLL and not containsLongestLine) or
+                (not shouldContainLL and containsLongestLine) ):
+                rcoords = list(reversed(coords))
+                startpointfirst = len(coords)-startpoint-1
+                startpointfirst = rcoords[startpointfirst:-1] + [rcoords[-1]] + rcoords[0:startpointfirst]
+
+
+        path = []
+        for i in range(-1+len(startpointfirst)):
+            path.append([startpointfirst[i], startpointfirst[i+1]])
+
+            if startpointfirst[i+1]==coords[endpoint]:
+                break
+        
+        return path
+    
+
+def findLinePaths(polygon):
     """Returns linePathX and linePathY which are each associated with a parallel/perpendicular line.
 
     Returns [ (linePathX, gradient) , (linePathY, gradient) ]
@@ -74,7 +125,7 @@ def findAdjacentLines(polygon):
 
     coords = polygon.exterior.coords[:-1]
     xmin = xmax = ymin = ymax = 0
-    
+
     for idx in range(len(coords)):
         if coords[idx][X] < coords[xmin][X]:
             xmin = idx
@@ -90,86 +141,25 @@ def findAdjacentLines(polygon):
     mparallel, c, isV = LineFunctions.lineEQ(longestline[linep1idx], longestline[linep2idx])
     mperp = -1/mparallel
 
-    if (-1 < mparallel < 1) or True:
+    if (-1 < mparallel < 1):
         # parallel line is more horizontal
         #   so the parallel line will be for the ypath and ypath will not include the longest line
         #   and the perpendicular line will be for the xpath and xpath will include the longest line.
         
+        ypath = find_path(coords=coords, startpoint=ymax, endpoint=ymin, longestline=longestline, pathIsHorizontal=False, shouldContainLL=False)
+        xpath = find_path(coords=coords, startpoint=xmin, endpoint=xmax, longestline=longestline, pathIsHorizontal=True, shouldContainLL=True)
+            
 
-        def find_path(startpoint, endpoint, longestline):
-            """Returns a list of coordinate lines between the given points."""
-            pass
-
-        
-        if longestline[linep2idx][Y] > longestline[linep1idx][Y]:
-            longestline = [ longestline[linep2idx], longestline[linep1idx] ]
-
-
-        if ymax > ymin:
-            ymaxfirst = coords[ymax:-1] + [coords[-1]] + coords[0:ymax]
-        else:
-            ymaxfirst = coords[ymax:ymin+1]
-
-        for i in range(-1+len(ymaxfirst)):
-            line = [ymaxfirst[i], ymaxfirst[i+1]]
-            if line==[c for c in longestline]:
-                # This path contains the longest line, so have to change it.
-
-                rcoords = list(reversed(coords))
-                rymax = len(coords)-ymax-1
-                ymaxfirst = rcoords[rymax:-1] + [rcoords[-1]] + rcoords[0:rymax]
-
-        ypath = []
-        for i in range(-1+len(ymaxfirst)):
-            ypath.append([ymaxfirst[i], ymaxfirst[i+1]])
-
-            if ymaxfirst[i+1]==coords[ymin]:
-                break
-        
-        
-
-
-        if longestline[linep2idx][X] < longestline[linep1idx][X]:
-            longestline = [ longestline[linep2idx], longestline[linep1idx] ]
-
-        if xmin > xmax:
-            xminfirst = coords[xmin:-1] + [coords[-1]] + coords[0:xmin]
-        else:
-            xminfirst = coords[xmin:xmax+1]
-        
-
-        containsLongest = False
-        for i in range(-1+len(xminfirst)):
-            line = [xminfirst[i], xminfirst[i+1]]
-            if line==[c for c in longestline]:
-                # This path contains the longest line, so don't have to change it.
-                containsLongest = True
-                
-        if not containsLongest:
-            # change this path
-                rcoords = list(reversed(coords))
-                rxmin = len(coords)-xmin-1
-                xminfirst = rcoords[rxmin:-1] + [rcoords[-1]] + rcoords[0:rxmin]
-        
-
-        xpath = []
-        for i in range(-1+len(xminfirst)):
-            xpath.append([xminfirst[i], xminfirst[i+1]])
-
-            if xminfirst[i+1]==coords[xmax]:
-                break
-                
-                
-        # repeat the above process for the xpath, then repeat all of this for the else case.
-
-
-        
+        # now repeat all of this for the else case.
         
     else:
         # parallel line is more vertical
         #   so the parallel line will be for the xpath and xpath will not include the longest line
-        pass
+        #   and the perpendicular line will be for the ypath and ypath will include the longest line.
 
+        ypath = find_path(coords=coords, startpoint=ymax, endpoint=ymin, longestline=longestline, pathIsHorizontal=False, shouldContainLL=True)
+        xpath = find_path(coords=coords, startpoint=xmin, endpoint=xmax, longestline=longestline, pathIsHorizontal=True, shouldContainLL=False)
+        
     import geopandas
     import matplotlib.pyplot as plt
     fig, ax = plt.subplots()
@@ -199,7 +189,7 @@ rlp = rlp.to_crs(epsg=27700)
 rlp["name"] = ["rlp"]
 rlppolygon = rlp.geometry[0]
 
-findAdjacentLines(rlppolygon)
+findLinePaths(rlppolygon)
 
 
 
