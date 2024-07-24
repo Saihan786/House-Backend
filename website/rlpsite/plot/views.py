e0ac1ad15c4019979d901de348d490b392ccb7c1
Saihan786 <marshall.s15@hotmail.com>
2024-07-24 09:00:23 +0100

    Pass map template file to index page
    * Part of process of showing map on website

--- website/rlpsite/plot/views.py
+++ website/rlpsite/plot/views.py
@@ -2,7 +2,24 @@ from django.shortcuts import render
 from django.template import loader
 
 from django.http import HttpResponse
+# from . import 
+
+import webbrowser
+import os
+
 
 def index(request):
+    embedmap = loader.get_template("plot/map.html")
     template = loader.get_template("plot/index.html")
-    return HttpResponse(template.render(context=None, request=request))
+    context = {
+        "map": embedmap,
+    }
+
+
+
+    
+
+
+    
+
+    return HttpResponse(template.render(context=context, request=request))
