a6698bd377659c60fbf7ea86609ffdfbb08f9781
Saihan786 <marshall.s15@hotmail.com>
2024-07-24 10:26:44 +0100

    Add map to index page using staticfiles

--- website/rlpsite/plot/views.py
+++ website/rlpsite/plot/views.py
@@ -2,24 +2,11 @@ from django.shortcuts import render
 from django.template import loader
 
 from django.http import HttpResponse
-# from . import 
 
-import webbrowser
-import os
 
 
 def index(request):
-    embedmap = loader.get_template("plot/map.html")
     template = loader.get_template("plot/index.html")
-    context = {
-        "map": embedmap,
-    }
 
 
-
-    
-
-
-    
-
-    return HttpResponse(template.render(context=context, request=request))
+    return HttpResponse(template.render(context=None, request=request))
