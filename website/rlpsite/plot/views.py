1ad1344c0a2bf50eb0a600758c3f6018b7ea34ed
Saihan786 <marshall.s15@hotmail.com>
2024-07-25 15:18:34 +0100

    Add basic form and move plotting pyfiles

--- website/rlpsite/plot/views.py
+++ website/rlpsite/plot/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.template import loader
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
-from ....software import HRGenerator
+from .software import PlotOptimals, HRGenerator, LineFunctions, PolygonFunctions, RedLinePlot
+# from .software import HRGenerator
+from .software.PlotOptimals import plotProportions
+from .forms import RegionForm
 
 
 
@@ -11,3 +14,26 @@ def index(request):
     template = loader.get_template("plot/index.html")
 
     return HttpResponse(template.render(context=None, request=request))
+
+
+def generate(request):
+    
+    if request.method == "POST":
+        form = RegionForm(request.POST)
+
+
+
+        print(request.POST)
+        
+        
+        
+        
+        
+        
+        if form.is_valid():
+            return HttpResponseRedirect("/thanks/")
+    else:
+        form = RegionForm()
+
+
+    return render(request, "plot/generate.html", {"form": form})
