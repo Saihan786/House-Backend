4e718ed7939bf18304f218d2ab67a53c1f8e8cac
Saihan786 <marshall.s15@hotmail.com>
2024-07-25 13:46:28 +0100

    Add model for datageojson files and migrations

--- website/rlpsite/plot/views.py
+++ website/rlpsite/plot/views.py
@@ -3,10 +3,11 @@ from django.template import loader
 
 from django.http import HttpResponse
 
+from ....software import HRGenerator
+
 
 
 def index(request):
     template = loader.get_template("plot/index.html")
-
 
     return HttpResponse(template.render(context=None, request=request))
--- website/rlpsite/rlpsite/settings.py
+++ website/rlpsite/rlpsite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'plot.apps.PlotConfig',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
