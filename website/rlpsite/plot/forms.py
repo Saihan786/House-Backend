6114b31c4b393b93a32b9f4cc383340d3d1e694e
Saihan786 <marshall.s15@hotmail.com>
2024-07-29 21:08:30 +0100

    Add bootstrap5 to later style

--- website/rlpsite/plot/forms.py
+++ website/rlpsite/plot/forms.py
@@ -1,5 +1,9 @@
 from django import forms
 
+class ExampleForm(forms.Form):
+    title = forms.CharField(max_length=50)
+
+
 class RegionForm(forms.Form):
     title = forms.CharField(max_length=50)
     regionfile = forms.FileField()
--- website/rlpsite/plot/views.py
+++ website/rlpsite/plot/views.py
@@ -1,23 +1,31 @@
+import bootstrap5.forms
 from django.shortcuts import render
 from django.template import loader
 
 from django.http import HttpResponse, HttpResponseRedirect
 
 from .software.PlotOptimals import startplot
-from .forms import RegionForm
+from .forms import RegionForm, ExampleForm
 
 import geopandas
 import mpld3
 
-
-
+import bootstrap5
 
 
 
 def index(request):
     template = loader.get_template("plot/index.html")
-
-    return HttpResponse(template.render(context=None, request=request))
+    context = None
+    
+    # if request.method == "POST":
+    #     form = ExampleForm()
+    #     context = {"form": form}
+    # else:
+    #     form = ExampleForm()
+    #     context = {"form": form}
+    
+    return HttpResponse(template.render(context=context, request=request))
 
 
 def generate(request):
@@ -29,25 +37,6 @@ def generate(request):
             file = request.FILES["regionfile"].file
             rlp = geopandas.read_file(file)
             fig, houses = startplot(rlp)
-
-            # fig, ax = plt.subplots()
-            # points = ax.plot(range(10), 'o')
-            # labels = ['<h1>{title}</h1>'.format(title=i) for i in range(10)]
-            # plugins.connect(fig, plugins.PointHTMLTooltip(points, labels))
-
-            # gc = GeometryCollection(houses)
-            # labels = ['<h1>{title}</h1>'.format(title=i) for i in range(350)]
-            # plugins.connect(fig, plugins.PointHTMLTooltip(gc, labels))
-
-
-            # df = geopandas.GeoDataFrame()
-            # df.price = 
-            # fig, ax = plt.subplots()
-            # axes = df.price.plot(ax=ax, ls='', marker='.')
-            # labels = df.labels.values()
-            # tooltip = mpld3.plugins.PointLabelTooltip(axes.get_lines()[0], labels=labels)
-            # mpld3.plugins.connect(fig, tooltip)
-
 
             mp = mpld3.plugins.MousePosition(fontsize=12, fmt='.6g')
             mpld3.plugins.connect(fig, mp)
