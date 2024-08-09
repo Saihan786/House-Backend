3008b178f88578bddbbd99750957518137f02c9b
Saihan786 <marshall.s15@hotmail.com>
2024-08-09 18:41:05 +0100

    Standardise dxf centroid at origin

--- website/rlpsite/plot/software/InputBlocks.py
+++ website/rlpsite/plot/software/InputBlocks.py
@@ -19,6 +19,24 @@ house = None
 X,Y = 0,1
 
 
+def centerDXFAtOrigin(dxf):
+    """Moves the dxf until the origin is the mean of all of its points.
+
+    Exists the option to show the translation, which is only effective if the distance translated is not too large.
+    
+    This assumes that the garden
+    
+    *****ONLY WORKS FOR GDFs*****
+    
+    """
+
+    center = unary_union(dxf.geometry).centroid
+    def move(polygon):
+        shiftcoords = [(coord[X]-center.x, coord[Y]-center.y) for coord in polygon.exterior.coords[:-1]]
+        return Polygon(shiftcoords)
+    dxf.geometry = dxf.geometry.apply(move)
+
+
 def set_up_colors(dxfblock):
     """Adds a colors column to dxfblock which can be passed to the plot() method.
     
@@ -55,6 +73,7 @@ def readDXF():
     dxfblock['geom_type']= dxfblock.geometry.type
     dxfblock = dxfblock.loc[dxfblock.geom_type == 'Polygon']
     set_up_colors(dxfblock=dxfblock)
+    centerDXFAtOrigin(dxf=dxfblock)
 
     gardens = dxfblock.loc[dxfblock.Layer == 'GARDEN'].geometry
     parking = dxfblock.loc[dxfblock.Layer == 'PARKING'].geometry
@@ -84,12 +103,14 @@ def plotDXF(dxfblock, ax=None):
         geopandas.GeoSeries(parking).plot(ax=ax, color="grey")
 
 
-def dxf_parallel_to_ll(dxf, point_about_rotation=None, longestline=None):
+def dxf_parallel_to_ll(dxf, origin=True, point_about_rotation=None, longestline=None):
     """Rotates the gdf until the house is parallel to the resultLine.
 
     The rotation is about the given point or the origin if it is not given.
     
     Returns the updated dxf.
+
+    Normally moves the dxf to the origin after the operation.
 
     *****THIS ASSUMES THE DXF IS AT THE ORIGIN*****
     
@@ -110,12 +131,17 @@ def dxf_parallel_to_ll(dxf, point_about_rotation=None, longestline=None):
 
     dxf.geometry = dxf.rotate(resultAngle-changeAngle, point_about_rotation, use_radians=True)
 
+    if origin:
+        centerDXFAtOrigin(dxf=dxf)
 
-def rotateNinety(dxf, point_about_rotation=None):
+
+def rotateNinety(dxf, origin=True, point_about_rotation=None):
     """Rotates the given dxf 90 degrees.
     
     To be used after the dxf is parallel to the longest line, to make sure
     it is facing the road.
+
+    Normally moves the dxf to the origin after the operation.
     
     """
 
@@ -123,6 +149,9 @@ def rotateNinety(dxf, point_about_rotation=None):
             point_about_rotation=(0,0)
 
     dxf.geometry = dxf.rotate(90, point_about_rotation, use_radians=False)
+
+    if origin:
+        centerDXFAtOrigin(dxf=dxf)
     
 
 def get_dxf_as_gdf():
@@ -133,24 +162,6 @@ def get_dxf_as_gdf():
     """
 
     return dxfblock
-
-
-def centerDXFAtOrigin(dxf):
-    """Moves the dxf until the origin is the mean of all of its points.
-
-    Exists the option to show the translation, which is only effective if the distance translated is not too large.
-    
-    This assumes that the garden
-    
-    *****ONLY WORKS FOR GDFs*****
-    
-    """
-
-    center = unary_union(dxf.geometry).centroid
-    def move(polygon):
-        shiftcoords = [(coord[X]-center.x, coord[Y]-center.y) for coord in polygon.exterior.coords[:-1]]
-        return Polygon(shiftcoords)
-    dxf.geometry = dxf.geometry.apply(move)
 
 
 def get_dxf_separate():
